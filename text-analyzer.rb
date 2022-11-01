52692e15d88317c9017910a3a08becc39c0b6cd4
metasignal <timcollinsdesign+metasignal@gmail.com>
2022-10-31 22:57:00 -0400

    File open, line count calculation, character count, characters minus white space

--- text-analyzer.rb
+++ text-analyzer.rb
@@ -1,8 +1,22 @@
 # This is a text-analyzer 
 
+# creates an empty string for us to work with
+text = ""
 
-# load file
-File.open("oliver-twist.txt").each { |line| puts line }
+# sets a variable to zero
+line_count = 0
+
+# Calls the file class and runs the open method. Each line iterated over
+File.open("oliver-twist.txt").each do |line|
+  line_count = line_count + 1
+  text = text + line
+end
+
+puts "#{line_count} lines"
 
 
+total_characters = text.length 
+puts "#{total_characters} characters"
 
+total_characters_nospaces = text.gsub(/\s+/,'').length
+puts "#{total_characters_nospaces} characters excluding spaces"
