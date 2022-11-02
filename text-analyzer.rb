debc1ef8bd0549b884c20c30b27e3ee95c7a07b1
metasignal <timcollinsdesign+metasignal@gmail.com>
2022-11-01 21:29:28 -0400

    added character count excluding spaces with the gsub string method

--- text-analyzer.rb
+++ text-analyzer.rb
@@ -18,5 +18,9 @@ puts "#{line_count} lines"
 total_characters = text.length 
 puts "#{total_characters} characters"
 
+# gsub does a global search and replace 
+# you can use gsub to eradicate spaces in the text string
 total_characters_nospaces = text.gsub(/\s+/,'').length
 puts "#{total_characters_nospaces} characters excluding spaces"
+
+
