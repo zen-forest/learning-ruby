a6468209ca11fc77a51d951673bb73831abd37d4
metasignal <timcollinsdesign+metasignal@gmail.com>
2022-11-08 21:33:00 -0500

    went through all concepts of object-orientation pre deep-dive

--- chapter-6.rb
+++ chapter-6.rb
@@ -1,4 +1,6 @@
 
+# ---
+
 # Classes
 # A class is a collection of methods and data that are used as a blueprint to create multiple objects relating to that class. 
 
@@ -6,3 +8,38 @@
 # An object is a single instance of a class. An object of class Person is a single person. An object of Dog is a single dog. If you think of 
 # objects as real-life objects, a class is the classification, whereas the object is the actual object or "thing" itself.
 
+# Local Variables
+# A variable that can only be accessed and used from the current scope. 
+
+# Instance variable/object variable
+# A variable that can be accessed and used from the scope of a single object. An object's methods can all access that objects object variables. 
+
+# Global Variable
+# A variable that can be accessed and used from anywhere within the current program. 
+
+# Class variable
+# A variable that can be accessed and used within the scope of a class and all of its child objects. 
+
+# Encapsulation
+# The concept of objects containing both data and methods that operate on that data, as well as allowing those methods to have differing degrees of 
+# visibility outside of their class or associated object. 
+
+# Polymorphism
+# The concept of methods being able to deal with different classes of dat and offering a more genereric implementation.
+
+# Module
+# An organizational element that collects together any number of classes, methods, and constants into a single namespace. 
+
+# Namespace
+# A named element of organization that keeps classes, methods, and constants from clashing. 
+
+# Mix-in
+# A module that can mix its methods into a class to extend that class's functionality
+
+# Enumerable
+# A mix-in modulem provided as standard with Ruby, that implements iterators and list-related methods for other classes, such as 
+# collect, map, min, and max. Ruby uses this module by default with Array and Hash classes.
+
+# Comparable
+# A mix-in module, provided as standard with Ruby, that implements comparison operators (such as <,>, and ==) on classes that 
+# implement the generic comparison operator <=>.
