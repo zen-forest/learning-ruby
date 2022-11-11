7b50ed2bd2cfafbf49bb5471e6cda27f25e845bc
metasignal <timcollinsdesign+metasignal@gmail.com>
2022-11-11 16:43:09 -0500

    overriding existing methods

--- chapter-6.rb
+++ chapter-6.rb
@@ -164,6 +164,41 @@ end
 
 p = Person.new("Bill")
 puts p.name
+# Very cool. "super" looks up the inheritance chain and allows to reference that method within a new context
+# The benefits of using inheritance this way is that you can implement generic functionality in generic classes and then implement only the specific functionality
+# that is more specific child classes equire. This saves a lot of repetition and means that if you make changes to parent classes, child classes will inherit these changes too. 
+# When you have hundreds of different classes in an application, it pays to cut down on repetition! 
+
+# Overriding Existing Methods 
+# let's override Ruby's String class
+
+x = "this is a test"
+puts x.class
+
+# String methods
+puts x.length
+puts x.upcase
+
+class String
+  def length
+    20
+  end
+end
+
+puts "This is a test".length
+puts "a".length
+puts "A really long line of text".length
+
+# Some libraries and extensions to Ruby override the methods supplied by the core classes to 
+# extend the functionality of Ruby in general. 
+# tread with caution 
+# you can override your own methods
+
+class Dog
+  def talk
+    puts "Woof!"
+  end
+end
 
 # ---
 
