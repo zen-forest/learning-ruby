3ed3426d43f048bfbc95cc18a1b7edf51d4b75f3
metasignal <timcollinsdesign+metasignal@gmail.com>
2022-11-14 20:40:50 -0500

    Covered polymorphism, fun big word for simple concept

--- chapter-6.rb
+++ chapter-6.rb
@@ -286,7 +286,37 @@ class Person4
 end
 puts Person4.methods
 
+# Polymorphism
+# Polymorphism is the concept of writing code that can work with objects of multiple types and classes at once. 
 
+class Animal
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Cat < Animal
+  def talk
+    "Meow!"
+  end
+end
+
+class Dog1 < Animal
+  def talk
+    "Woof!"
+  end
+end
+
+animals = [Cat.new("Flossie"), Dog1.new("Clive"), Cat.new("Max")]
+animals.each do |animal|
+  puts animal.talk
+end
+# This shows how you can loop over and work on obojects of different classes, but get expected results in each case if each class implements the same methods. 
+# Some of Ruby's built-in standard classes (such as Array, Hash, String) have polymorphic methods built-in classes to return the contents of the object as a string
+puts 1000.to_s
+puts [1,2,3].to_s
+puts ({name: "Fred", age: 10}).to_s
 
 # ---
 
