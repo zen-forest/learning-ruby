15d9284f6e97e69f9d1abbfa6598cbf56ece34de
metasignal <timcollinsdesign+metasignal@gmail.com>
2022-11-21 23:28:47 -0500

    Intro to Enumerable

--- chapter-6.rb
+++ chapter-6.rb
@@ -484,6 +484,19 @@ AnotherModule.do_stuff
 # The kernel module contains all the "standard" commands you use in Ruby such as load, require, exit, puts, and eval
 # Ruby has  modules that you can include in your own classes to gain more functionality immediately. Two such are: Enumerable and Comparable
 
+# Enumerable
+[1,2,3,4,5].each { |number| puts number }
+# In this case you create a temporary array and use the each to iterate over each value in the code block
+
+my_array = %w{this is a test of the longest word check}
+longest_word = ''
+my_array.each do |word|
+  longest_word = word if longest_word.length < word.length 
+end
+
+puts longest_word 
+
+
 
 # ---
 
